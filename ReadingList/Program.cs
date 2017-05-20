cc1a75de66ae9334d7ed7ff01076de2cf391fc7b
sebug <sebastian.gfeller@gmail.com>
2017-05-20 17:27:53 +0200

    Up until Web API with https

---
+++ ReadingList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ReadingList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(options => {
+                    options.UseHttps("localhost.pfx", "Elegants Meitschi");
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseUrls("https://*:44332")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
